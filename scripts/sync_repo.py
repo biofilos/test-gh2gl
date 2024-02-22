0ac7c7737c6d3eb4fcc6c0f5aeb7517842576006
Juan Felipe Ortiz <jf.ortiz@nus.edu.sg>
2024-02-22 22:58:25 +0800

    refactor variables

--- scripts/sync_repo.py
+++ scripts/sync_repo.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         f.write(json.dumps(event))
     obj_key = event["Records"][0]["s3"]["object"]["key"]
     s3 = boto3.resource('s3')
-    event_json = f'{obj_key.split(".")[0]}.json'
+    event_json = f'{obj_key.split(".")[0].split("/")[-1]}.json'
     s3.Bucket('lb-gh2gl').upload_file('/tmp/repo.json', event_json)
     return {
         'statusCode': 200,
