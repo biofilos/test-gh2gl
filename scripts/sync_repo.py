64439ae15b52a4611aa7ad82f89ac6199957afca
Juan Felipe Ortiz <jf.ortiz@nus.edu.sg>
2024-02-22 23:12:10 +0800

    use pathlib to parse file names

--- scripts/sync_repo.py
+++ scripts/sync_repo.py
@@ -1,13 +1,14 @@
 import json
 import boto3
-
+from pathlib import Path
 
 def lambda_handler(event, context):
     with open('/tmp/repo.json', 'w') as f:
         f.write(json.dumps(event))
-    obj_key = event["Records"][0]["s3"]["object"]["key"]
+    obj_key = Path(event["Records"][0]["s3"]["object"]["key"])
     s3 = boto3.resource('s3')
-    event_json = f'{obj_key.split(".")[0].split("/")[-1]}.json'
+    event_json = f'{obj_key.stem}.json'
+    print(f'Uploading to {event_json}')
     s3.Bucket('lb-gh2gl').upload_file('/tmp/repo.json', event_json)
     return {
         'statusCode': 200,
