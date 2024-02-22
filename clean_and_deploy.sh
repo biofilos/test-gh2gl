28e8d02fb38f3fe00f6e02712d01c6f28b649451
Juan Felipe Ortiz <jf.ortiz@nus.edu.sg>
2024-02-22 22:57:28 +0800

    dont delete stack on cleaning

--- clean_and_deploy.sh
+++ clean_and_deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 aws s3 rm s3://lb-gh2gl/README.md
-sam delete --stack-name upload-lambda --no-prompts
+#sam delete --stack-name upload-lambda --no-prompts
 sam package --template-file upload_lambda.yml --output-template-file pkg_lambda.yaml --s3-bucket lb-lambdas --region ap-southeast-1
 sam deploy --template-file pkg_lambda.yaml --stack-name upload-lambda --region ap-southeast-1 --capabilities CAPABILITY_IAM
 aws s3 cp README.md s3://lb-gh2gl/README.md
