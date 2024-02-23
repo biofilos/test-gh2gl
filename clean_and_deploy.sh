49139fedad73c9eca87f135c6f014da7c0c5eadd
Juan Felipe Ortiz <jf.ortiz@nus.edu.sg>
2024-02-23 19:58:07 +0800

    allow retreival of secrets

--- clean_and_deploy.sh
+++ clean_and_deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 REGION=ap-southeast-1
 SAM_DOC=upload_lambda.yml
@@ -6,5 +7,6 @@ PKG_DOC=pkg_lambda.yaml
 STACK_NAME=upload-lambda
 CF_BUCKET=lb-lambdas
 
-sam package --template-file ${SAM_DOC} --output-template-file ${PKG_DOC} --s3-bucket ${CF_BUCKET} --region ${REGION}
+sam build -t ${SAM_DOC}
+sam package --output-template-file ${PKG_DOC} --s3-bucket ${CF_BUCKET} --region ${REGION}
 sam deploy --template-file ${PKG_DOC} --stack-name ${STACK_NAME} --region ${REGION} --capabilities CAPABILITY_IAM
--- fx/sync_repo.py
+++ fx/sync_repo.py
@@ -2,6 +2,34 @@ import json
 import boto3
 from pathlib import Path
 import gitlab
+from aws_lambda_powertools.utilities import parameters
+import os
+
+import boto3
+
+# def get_secret(secret_name, region_name):
+#     session = boto3.session.Session()
+#     client = session.client(
+#         service_name='secretsmanager',
+#         region_name=region_name
+#     )
+#
+#     get_secret_value_response = client.get_secret_value(
+#         SecretId=secret_name
+#     )
+#
+#     if 'SecretString' in get_secret_value_response:
+#         secret = get_secret_value_response['SecretString']
+#     else:
+#         secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+#
+#     return secret
+#
+# # Usage
+# secret_name = "gitlab_token"
+# region_name = "us-west-2"  # replace with your AWS region
+
+# gl_private_token = get_secret(secret_name, region_name)
 
 def lambda_handler(event, context):
     with open('/tmp/repo.json', 'w') as f:
@@ -14,8 +42,15 @@ def lambda_handler(event, context):
 
     event_json = f'{obj_key.stem}.json'
     print(f'Uploading to {event_json}')
-    s3.Bucket('lb-gh2gl').upload_file('/tmp/repo.json', event_json)
+    # s3.Bucket('lb-gh2gl').upload_file('/tmp/repo.json', event_json)
+
+    ## Gitlab section
+    gl_domain = "https://gitlab.com"
+    gl_private_token = json.loads(parameters.get_secret("gitlab_token"))
+    gl = gitlab.Gitlab(gl_domain, private_token=gl_private_token["gl_token"])
+    gl.auth()
+
     return {
         'statusCode': 200,
-        'event': json.dumps(event)
+        'event': f"Pipelines {obj_key.stem}"
     }
