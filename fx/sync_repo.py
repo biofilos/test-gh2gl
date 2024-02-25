37bfb73d544bbd5445f32f93d8b222778ce802f1
Juan Felipe Ortiz <jf.ortiz@nus.edu.sg>
2024-02-25 13:11:47 +0800

    wait until latest pipeline finishes

--- fx/sync_repo.py
+++ fx/sync_repo.py
@@ -28,25 +28,30 @@ def lambda_handler(event, context):
     ## Gitlab section
     gl_domain = "https://gitlab.com"
     gl_private_token = json.loads(parameters.get_secret("gitlab_token"))["gl_token"]
-    gl = gitlab.Gitlab(gl_domain, private_token=gl_private_token)
-    gl.auth()
-    project = gl.projects.get(gitlab_repo)
-    trigger = project.triggers.create({
-        "description": "Triggered by lambda"
-    })
-    vars = {
-        "s3_path": f"s3://{bucket}/{obj_key_str}",
-        "gitlab_path": obj_key_str, "run_now": "yes",
-        "access_token": gl_private_token,
-        "branch": branch,
-        "gitlab_repo": gitlab_repo
-    }
-
-    pipeline = project.trigger_pipeline(branch, trigger.token, variables=vars)
-    while pipeline.finished_at is None:
-        pipeline.refresh()
-        time.sleep(1)
-
+    # Login to Gitlab
+    with gitlab.Gitlab(gl_domain, private_token=gl_private_token) as gl:
+        gl.auth()
+        # Select repo
+        project = gl.projects.get(gitlab_repo)
+        trigger = project.triggers.create({
+            "description": "Triggered by lambda"
+        })
+        # These variables are used in the .gitlab-ci.yml file
+        vars = {
+            "s3_path": f"s3://{bucket}/{obj_key_str}",
+            "gitlab_path": obj_key_str, "run_now": "yes",
+            "access_token": gl_private_token,
+            "branch": branch,
+            "gitlab_repo": gitlab_repo
+        }
+        # Wait until the latest pipeline is finished
+        while project.pipelines.list(ref=branch, status="running") or project.pipelines.list(ref=branch, status="pending"):
+            time.sleep(1)
+        pipeline = project.trigger_pipeline(branch, trigger.token, variables=vars)
+        while pipeline.finished_at is None:
+            pipeline.refresh()
+            time.sleep(1)
+        trigger.delete()
     return {
         'statusCode': 200,
         'event': f"Pipelines {obj_key.stem}"
